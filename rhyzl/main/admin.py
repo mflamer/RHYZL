a82f6141869545f5e5aae6d1a9664e2c0e64b9d7
mschuldt <mbschuldt@gmail.com>
2019-07-09 14:34:33 -0700

    register models for admin site

--- rhyzl/main/admin.py
+++ rhyzl/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+for model in (Address, Employee, Strain, Plant, Facility, Farm):
+    admin.site.register(model)
